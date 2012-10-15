56b71f22ca2731284d1ca72dbc9e3ba9b7e6a060
Jussi Leinonen <jsleinonen@gmail.com>
2012-10-15 18:49:58 +0300

    Reorganized for installer

---
+++ pymiecoated/__init__.py
@@ -0,0 +1,1 @@
+from mie_coated import Mie
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+long_description = """A Python code for computing the scattering properties
+of single- and dual-layered spheres with an easy-to-use object oriented
+interface.
+
+Based on code by C. Mätzler; ported and published with permission.
+
+Requires NumPy and SciPy.
+"""
+
+setup(name='pymiecoated',
+      version='0.1.0',
+      description='Single- and dual-layered Mie scattering computations',
+      author='Jussi Leinonen',
+      author_email='jsleinonen@gmail.com',
+      url='http://code.google.com/p/pymiecoated/',      
+      packages=['pymiecoated','pymiecoated.demos','pymiecoated.test'],
+      license='MIT',
+      long_description = long_description,
+     )
